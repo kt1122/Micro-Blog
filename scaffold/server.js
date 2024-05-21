6c3e422de2ce48fb9960fecf4c2acf9f696a3bf4
Maia Burton <maiameiling@gmail.com>
2024-05-21 15:08:31 -0700

    minor changes

--- scaffold/server.js
+++ scaffold/server.js
@@ -254,7 +254,7 @@ app.listen(PORT, () => {
 
 // Example data for posts and users
 let posts = [
-    { id: 1, title: 'igpay atinlay', content: 'isthay isyay ayay amplesay ostpay.', username: 'I\'veBuriedMyClocks', timestamp: '2024-01-01 10:00', likes: 0 },
+    { id: 1, title: 'igpay atinlay', content: 'isthay isyay ayay amplesay ostpay.', username: 'SampleUser', timestamp: '2024-01-01 10:00', likes: 0 },
     { id: 2, title: 'What did the fish say when he hit a wall?', content: 'Damn.', username: 'AlwaysMeasureTwiceCutOnce', timestamp: '2023-07-25 7:00', likes: 10 },
     { id: 3, title: 'The Latin Pig?', content: 'Not much actual pig themed content here, huh?', username: 'Don\'tFightThePickles', timestamp: '2023-07-21 7:45', likes: 1 },
     { id: 4, title: '???', content: 'I don\'t *know* pig latin!', username: 'Don\'tFightThePickles', timestamp: '2023-07-30 4:45', likes: 1 },
@@ -266,7 +266,8 @@ let users = [
     { id: 1, username: 'I\'veBuriedMyClocks', avatar_url: undefined, memberSince: '2024-01-01 08:00' },
     { id: 2, username: 'LeaveTheRatsAlone', avatar_url: undefined, memberSince: '2024-01-02 09:00' },
     { id: 3, username: 'Don\'tFightThePickles', avatar_url: undefined, memberSince: '2023-01-15 06:00' },
-    { id: 4, username: 'AlwaysMeasureTwiceCutOnce', avatar_url: undefined, memberSince: '2021-09-21 010:00' },
+    { id: 4, username: 'AlwaysMeasureTwiceCutOnce', avatar_url: undefined, memberSince: '2021-09-21 10:00' },
+    { id: 5, username: 'SampleUser', avatar_url: undefined, memberSince: '2021-03-19 10:00' },
 ];
 
 // Function to find a user by username
